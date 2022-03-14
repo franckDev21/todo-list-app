7b4d92b94a431b470e64bb562f53b8ba6f7956be
franckDev21 <tutodevphp@gmail.com>
2022-03-14 04:17:31 +0100

    adding dist

---
+++ js/about.88d2c1ce.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(n,t,a){"use strict";a.r(t);var e=a("7a23"),s={class:"about"},c=Object(e["g"])("h1",null,"This is an about page",-1),o=[c];function u(n,t){return Object(e["s"])(),Object(e["f"])("div",s,o)}var b=a("6b0d"),i=a.n(b);const r={},d=i()(r,[["render",u]]);t["default"]=d}}]);
+//# sourceMappingURL=about.88d2c1ce.js.map
